53ebb82eb2032cdd081cf7af7bb51dab45bb0cba
Mobarak Ali <mobarak.ali@gmail.com>
2023-04-04 21:07:43 +0600

    Assignment 09 is OK now!

---
+++ assignments/module09/process.php
@@ -0,0 +1,42 @@
+<?php
+$title = "Contact Page";
+$page = "contact";
+include_once 'includes/Header.php';
+?>
+<!--Contact Page Heading -->
+<div class="p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-4 fw-normal">Contact Us</h1>
+        </div>
+</div>
+<!--Code For Contact page-->
+<div class="container" style="min-height:60vh">
+    <div class="row justify-content-center">
+        <div  class="col-md-8 py-5">
+            <form method="POST" action="process-form.php">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name<span class="text-danger">*</span></label>
+                    <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address<span class="text-danger">*</span></label>
+                    <input type="email" name="email" class="form-control" id="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="subject" class="form-label">Subject<span class="text-danger">*</span></label>
+                    <input type="text" name="subject" class="form-control" id="subject" required>
+                </div>
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message<span class="text-danger">*</span></label>
+                    <textarea name="message" class="form-control" id="message" rows="5" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once 'includes/Footer.php';
+?>
+
---
+++ assignments/module09/single.php
@@ -0,0 +1,123 @@
+<?php
+$title = "Our Blog";
+include_once 'includes/Header.php';
+?>
+<!--Code For Blog page-->
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-4 fw-normal">Our Blogs</h1>
+        </div>
+    </div>
+<!-- List of Blogs-->
+<main class="container py-5">
+    <div class="row g-5">
+        <div class="col-md-9">
+            <section class="container pb-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="https://picsum.photos/600/400?" alt="Featured Post Image" class="img-fluid mb-3">
+                    </div>
+                    <div class="col-md-6">
+                        <h3 class="mb-3">Latest Blog Post Title</h3>
+                        <p class="lead">Lorem Aipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ligula a suscipit tristique. Donec gravida velit vel sagittis fermentum. Proin malesuada pharetra mauris ac euismod. Vivamus tincidunt mauris sed ipsum hendrerit laoreet.</p>
+                        <a href="#" class="btn btn-dark">Read More</a>
+                    </div>
+                </div>
+            </section>
+
+            <div class="row mb-4">
+                <div class="col">
+                    <div class="card">
+                        <img src="https://picsum.photos/250" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Blog title</h5>
+                            <p class="card-text">Some Bquick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://picsum.photos/250?blur" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Blog title</h5>
+                            <p class="card-text">Some Cquick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://picsum.photos/250?grayscale" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Blog title</h5>
+                            <p class="card-text">Some Dquick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/250.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Blog title</h5>
+                            <p class="card-text">Some Equick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/250.webp" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Blog title</h5>
+                            <p class="card-text">Some Fquick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/250.jpg?grayscale" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Blog title</h5>
+                            <p class="card-text">Some Gquick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="position-sticky" style="top: 2rem;">
+                <form class="d-flex mt-lg-0" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-dark" type="submit">Search</button>
+                </form>
+
+                <div class="p-4">
+                    <h4 class="fst-italic">Categories</h4>
+                    <ol class="list-unstyled">
+                        <li><a href="#">Lavavel</a></li>
+                        <li><a href="#">HTML</a></li>
+                        <li><a href="#">CSS</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+<!--        -->
+    </div>
+</main>
+
+
+
+<?php
+include_once 'includes/Footer.php';
