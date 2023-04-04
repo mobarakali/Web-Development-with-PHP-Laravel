9bf21727cbc19cddb91b2da459b27ea12291bd14
Mobarak Ali <mobarak.ali@gmail.com>
2023-04-04 21:12:00 +0600

    updating category

--- assignments/module09/index.php
+++ assignments/module09/index.php
@@ -20,7 +20,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/assignments/module09/includes/header.
         <div class="col-md-6">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">World</strong>
+                    <strong class="d-inline-block mb-2 text-primary">CSS</strong>
                     <h3 class="mb-0">Featured post</h3>
                     <div class="mb-1 text-muted">Nov 12</div>
                     <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -34,7 +34,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/assignments/module09/includes/header.
         <div class="col-md-6">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-success">Design</strong>
+                    <strong class="d-inline-block mb-2 text-success">HTML</strong>
                     <h3 class="mb-0">Post title</h3>
                     <div class="mb-1 text-muted">Nov 11</div>
                     <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
